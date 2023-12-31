898705e50882072805dbd8ccef704957e41759ba
Anupam <114251464+9582anupam@users.noreply.github.com>
2024-01-01 00:27:22 +0530

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    // printf("Hello, world");
+    int i;
+    for (++i; i<6; i++)
+    {
+        i++;
+        printf("%d ", i);
+    }
+    // printf("Hello, world");
+}
